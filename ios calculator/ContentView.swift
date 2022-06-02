f2796cf769844255395c5ea572c1b324a7a907b1
Long Do <longdt12@onemount.com>
2022-06-03 00:49:48 +0700

    Add Calculator

--- ios calculator/ContentView.swift
+++ ios calculator/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var total = "100"
+    @State var tipPercent = 15.0
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Tip Calculator")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+                
+            
+            HStack {
+                Text("$")
+                TextField("Amount", text: $total)
+            }
+            .padding(.all)
+            
+            HStack {
+                Slider(value: $tipPercent, in: 1...30, step: 1.0)
+                Text("\(Int(tipPercent))%")
+            }
             .padding()
+            
+            if let totalNum = Double(total) {
+                Text("Tip Amount: $\(totalNum * tipPercent / 100.0, specifier: "%0.2f")")
+            } else {
+                Text("Please enter a numberical value")
+            }
+        }
     }
 }
 
