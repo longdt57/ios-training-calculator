fedf0726312e1e045663f0c665344548d5be4c87
Long Do <longdt12@onemount.com>
2022-06-02 23:52:02 +0700

    Initial Commit

---
+++ ios calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios calculator
+//
+//  Created by LONGDT12 on 6/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios calculator/ios_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_calculatorApp.swift
+//  ios calculator
+//
+//  Created by LONGDT12 on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ios_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
